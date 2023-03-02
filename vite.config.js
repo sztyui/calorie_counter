f7f02614be13642440fe7def44493d8fd9ccf6b6
Szilágyi István <i10815@eon.com>
2023-03-02 12:45:06 +0100

    vite.config.js modif

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import process from 'node:process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: process.env.NODE_ENV === 'production'
+  ? '/calorie_webapp/'
+  : '/',
 })
