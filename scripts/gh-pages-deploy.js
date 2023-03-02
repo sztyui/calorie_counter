f281eb2400e891cddadbf5f1ab67e3ab11a9b900
Szilágyi István <i10815@eon.com>
2023-03-02 09:15:15 +0100

    deploy script modif

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -16,7 +16,8 @@ import process from 'process';
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await execa("Remove-Item", ["-Recurse", folderName]);
+    // await execa("Remove-Item", ["-Recurse", folderName]);
+    fs.rmSync(folderName, { recursive: true, force: true })
     await execa("git", ["checkout", "-f", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed");
